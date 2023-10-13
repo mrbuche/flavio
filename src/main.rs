6d6a4cc11bebd232105725d61a53129174511ebb
mrbuche <dev@mrbuche.com>
2023-10-13 17:19:07 -0600

    TDD

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+fn main()
+{
+    println!("\n\n\x1b[1mf l a v i o\x1b[0m\n\n");
+    println!("\x1b[94mflavio welcomes you\x1b[0m\n\n");
+    println!("\x1b[96mSolving your problem...\x1b[0m\n");
+    println!("\x1b[95m    0 1 2 3 4 5 6 7 8 9\x1b[0m\n");
+    println!("\x1b[92mSuccess!\x1b[0m");
+    println!("\x1b[32mI have succeeded.\x1b[0m\n");
+    println!("\x1b[93mWarning!\x1b[0m");
+    println!("\x1b[33mI am warning you about something.\x1b[0m\n");
+    println!("\x1b[91mFailure!\x1b[0m");
+    println!("\x1b[31mI have failed.\x1b[0m\n");
+}
