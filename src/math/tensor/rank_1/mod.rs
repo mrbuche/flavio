668966f2ff6c1b7c9d548ba343ac411cbb012dac
mrbuche <dev@mrbuche.com>
2023-10-13 20:10:55 -0600

    I guess some things cannot be traits for now

--- src/math/tensor/rank_1/mod.rs
+++ src/math/tensor/rank_1/mod.rs
@@ -12,3 +12,30 @@ pub struct TensorRank1<const D: usize>
 (
     pub [TensorRank0; D]
 );
+
+
+// move into TensorRank1Traits if ever becomes possible
+impl<const D: usize> TensorRank1<D>
+{
+    fn iter(&self) -> impl Iterator<Item=&TensorRank0>
+    {
+        self.0.iter()
+    }
+    fn iter_mut(&mut self) -> impl Iterator<Item=&mut TensorRank0>
+    {
+        self.0.iter_mut()
+    }
+}
+
+pub trait TensorRank1Traits
+{
+    fn zero() -> Self;
+}
+
+impl<const D: usize> TensorRank1Traits for TensorRank1<D>
+{
+    fn zero() -> Self
+    {
+        TensorRank1([0.0; D])
+    }
+}
