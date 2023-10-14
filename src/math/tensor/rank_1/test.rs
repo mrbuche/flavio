30d01394723485cd59800183ce39d89417fe9c1b
mrbuche <dev@mrbuche.com>
2023-10-14 10:58:18 -0600

    test for copy if going to allow it

--- src/math/tensor/rank_1/test.rs
+++ src/math/tensor/rank_1/test.rs
@@ -1,6 +1,16 @@
 use super::TensorRank1;
 
 #[test]
+fn copy()
+{
+    let a = TensorRank1([1.0, 2.0, 3.0]);
+    let b = a;
+    a.iter().zip(b.iter()).for_each(|(a_i, b_i)|
+        assert_eq!(a_i, b_i)
+    );
+}
+
+#[test]
 fn todo()
 {
     todo!("Make comprehensive tests, and traits for <D> and <3> instead of base impls in order to use for mechanics.");
