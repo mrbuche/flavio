2261a9e9a195af25f3ad44adc40a0e5a7beee13f
mrbuche <dev@mrbuche.com>
2023-10-15 15:22:25 -0600

    working

--- src/math/tensor/rank_2/mod.rs
+++ src/math/tensor/rank_2/mod.rs
@@ -48,26 +48,144 @@ impl<const D: usize> TensorRank2<D>
 pub trait TensorRank2Traits<const D: usize>
 where
     Self: Index<usize, Output = TensorRank1<D>>
+        + IndexMut<usize, Output = TensorRank1<D>>
+        + Mul<Output = Self>
+        + Mul<&Self, Output = Self>
+        + Sized
 {
-    type Inverse;
-    fn inverse(&self) -> Self::Inverse
+    // not as big of a deal to have all the default trait implementations
+    // if just going to redo them in a struct-tuple deal as Tensor<D, I, J>
+    // only helps with things like the inverse, which want to be overridden for some specific <D> implementations here
+    fn frobenius_norm(&self) -> TensorRank0
     {
-        todo!("Can use the LU decomposition as the default implementation and override for certain dimensions.");
+        (self.transpose()*self).trace().sqrt()
     }
+    fn inverse(&self) -> Self
+    {
+        let (tensor_l, tensor_u) = self.lu_decomposition();
+        tensor_u.inverse_upper_triangular() * tensor_l.inverse_lower_triangular()
+    }
+    fn inverse_lower_triangular(mut self) -> Self
+    {
+        let mut sum;
+        for i in 0..D
+        {
+            self[i][i] = 1.0/self[i][i];
+            for j in 0..i
+            {
+                sum = 0.0;
+                for k in j..i
+                {
+                    sum += self[i][k] * self[k][j];
+                }
+                self[i][j] = -sum * self[i][i];
+            }
+        }
+        self
+    }
+    fn inverse_upper_triangular(mut self) -> Self
+    {
+        let mut sum;
+        for i in 0..D
+        {
+            self[i][i] = 1.0/self[i][i];
+            for j in 0..i
+            {
+                sum = 0.0;
+                for k in j..i
+                {
+                    sum += self[j][k] * self[k][i];
+                }
+                self[j][i] = -sum * self[i][i];
+            }
+        }
+        self
+    }
+    fn lu_decomposition(&self) -> (Self, Self);
     fn trace(&self) -> TensorRank0
     {
         (0..D).map(|i| self[i][i]).sum()
     }
-    fn transpose(&self) -> Self::Inverse
-    {
-        todo!();
-    }
+    fn transpose(&self) -> Self;
     fn zero() -> Self;
 }
 
 impl<const D: usize> TensorRank2Traits<D> for TensorRank2<D>
 {
-    type Inverse = Self;
+    fn lu_decomposition(&self) -> (Self, Self)
+    {
+        let mut tensor_l = Self::zero();
+        let mut tensor_u = Self::zero();
+        self.iter().enumerate().for_each(|(i, self_i)|
+        {
+            self.iter().enumerate().zip(tensor_l.iter_mut()).for_each(|((j, self_j), tensor_l_j)|
+            {
+                if j >= i
+                {
+                    tensor_l_j[i] = self_j[i];
+                    for k in 0..i
+                    {
+                        tensor_l_j[i] -= tensor_l_j[k] * tensor_u[k][i];
+                    }
+                }
+            });
+            self_i.iter().enumerate().for_each(|(j, self_ij)|
+            {
+                if j == i
+                {
+                    tensor_u[i][j] = 1.0;
+                }
+                else if j > i
+                {
+                    tensor_u[i][j] = self_ij / tensor_l[i][i];
+                    for k in 0..i
+                    {
+                        tensor_u[i][j] -= (tensor_l[i][k] * tensor_u[k][j]) / tensor_l[i][i];
+                    }
+                }
+            });
+        });
+        // for i in 0..D
+        // {
+        //     for j in 0..D
+        //     {
+        //         if j >= i
+        //         {
+        //             tensor_l[j][i] = self[j][i];
+        //             for k in 0..i
+        //             {
+        //                 tensor_l[j][i] -=  tensor_l[j][k] * tensor_u[k][i];
+        //             }
+        //         }
+        //     }
+        //     for j in 0..D
+        //     {
+        //         if j == i
+        //         {
+        //             tensor_u[i][j] = 1.0;
+        //         }
+        //         else if j > i
+        //         {
+        //             tensor_u[i][j] = self[i][j] / tensor_l[i][i];
+        //             for k in 0..i
+        //             {
+        //                 tensor_u[i][j] -= (tensor_l[i][k] * tensor_u[k][j]) / tensor_l[i][i];
+        //             }
+        //         }
+        //     }
+        // }
+        (tensor_l, tensor_u)
+    }
+    fn transpose(&self) -> Self
+    {
+        let mut transpose = Self::zero();
+        transpose.iter_mut().enumerate().for_each(|(i, transpose_i)|
+            transpose_i.iter_mut().enumerate().for_each(|(j, transpose_ij)|
+                *transpose_ij = self[j][i]
+            )
+        );
+        transpose
+    }
     fn zero() -> Self
     {
         Self(std::array::from_fn(|_| TensorRank1::zero()))
@@ -88,5 +206,40 @@ impl<const D: usize> IndexMut<usize> for TensorRank2<D>
     fn index_mut(&mut self, index: usize) -> &mut Self::Output
     {
         &mut self.0[index]
+    }
+}
+
+
+
+
+
+impl<const D: usize> Mul for TensorRank2<D>
+{
+    type Output = Self;
+    fn mul(self, tensor_rank_2: Self) -> Self::Output
+    {
+        let mut output = TensorRank2::zero();
+        let tensor_rank_2_transpose = tensor_rank_2.transpose();
+        output.iter_mut().zip(self.iter()).for_each(|(output_i, self_i)|
+            output_i.iter_mut().zip(tensor_rank_2_transpose.iter()).for_each(|(output_ij, tensor_rank_2_transpose_j)|
+                *output_ij = *self_i * tensor_rank_2_transpose_j
+            )
+        );
+        output
+    }
+}
+impl<const D: usize> Mul<&Self> for TensorRank2<D>
+{
+    type Output = Self;
+    fn mul(self, tensor_rank_2: &Self) -> Self::Output
+    {
+        let mut output = TensorRank2::zero();
+        let tensor_rank_2_transpose = tensor_rank_2.transpose();
+        output.iter_mut().zip(self.iter()).for_each(|(output_i, self_i)|
+            output_i.iter_mut().zip(tensor_rank_2_transpose.iter()).for_each(|(output_ij, tensor_rank_2_transpose_j)|
+                *output_ij = *self_i * tensor_rank_2_transpose_j
+            )
+        );
+        output
     }
 }
