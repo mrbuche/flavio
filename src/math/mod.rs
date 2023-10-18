cf42879a1706982d27576e706baa87e95d7cdf17
mrbuche <dev@mrbuche.com>
2023-10-18 11:24:01 -0600

    check next release docs.rs if this feature flag makes it through

--- src/math/mod.rs
+++ src/math/mod.rs
@@ -3,3 +3,4 @@ mod tensor;
 pub use tensor::rank_0::TensorRank0;
 pub use tensor::rank_1::TensorRank1;
 pub use tensor::rank_2::TensorRank2;
+pub use tensor::rank_4::TensorRank4;
--- src/math/tensor/mod.rs
+++ src/math/tensor/mod.rs
@@ -1,3 +1,4 @@
 pub mod rank_0;
 pub mod rank_1;
 pub mod rank_2;
+pub mod rank_4;
