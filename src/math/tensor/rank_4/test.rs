26208b6e1fa6ebda1c3e10641419212ec92ef35b
mrbuche <dev@mrbuche.com>
2023-10-18 09:52:54 -0600

    rank_4

---
+++ src/math/tensor/rank_4/test.rs
@@ -0,0 +1,14 @@
+use super::TensorRank4;
+
+#[test]
+fn tensor_rank_4()
+{
+    let _: TensorRank4<3>;
+}
+
+
+#[test]
+fn todo()
+{
+    todo!();
+}
--- tests/math.rs
+++ tests/math.rs
@@ -2,7 +2,8 @@ use flavio::math::
 {
     TensorRank0,
     TensorRank1,
-    TensorRank2
+    TensorRank2,
+    TensorRank4
 };
 
 // "Each file in the tests directory is a separate crate."
@@ -25,4 +26,10 @@ fn tensor_rank_1()
 fn tensor_rank_2()
 {
     let _: TensorRank2<3>;
+}
+
+#[test]
+fn tensor_rank_4()
+{
+    let _: TensorRank4<3>;
 }
