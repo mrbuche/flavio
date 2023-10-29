526fdb08d6461e5927daa629e2203091917401ac
mrbuche <dev@mrbuche.com>
2023-10-29 10:37:38 -0600

    need to test multiplicative

--- src/constitutive/hyperelastic/equal_deformation/test.rs
+++ src/constitutive/hyperelastic/equal_deformation/test.rs
@@ -1,17 +1,21 @@
-use crate::constitutive::
+use crate::
 {
-    GentModel,
-    MooneyRivlinModel,
-    NeoHookeanModel,
-    YeohModel,
-    hyperelastic::test::test_hyperelastic_constitutive_model_constructed,
-    test::
+    constitutive::
     {
-        GENTPARAMETERS,
-        MOONEYRIVLINPARAMETERS,
-        NEOHOOKEANPARAMETERS,
-        YEOHPARAMETERS
-    }
+        GentModel,
+        MooneyRivlinModel,
+        NeoHookeanModel,
+        YeohModel,
+        hyperelastic::test::test_hyperelastic_constitutive_model_constructed,
+        test::
+        {
+            GENTPARAMETERS,
+            MOONEYRIVLINPARAMETERS,
+            NEOHOOKEANPARAMETERS,
+            YEOHPARAMETERS
+        }
+    },
+    mechanics::test::get_deformation_gradient
 };
 use super::*;
 
@@ -55,7 +59,68 @@ mod nested
 }
 
 #[test]
-fn still_need_to_test_things_like_equal_stress_for_same_model()
+fn additive_cauchy_stress()
 {
-    todo!()
+    CompositeHyperelasticConstitutiveModelEqualDeformation::construct(
+        GentModel::new(GENTPARAMETERS),
+        MooneyRivlinModel::new(MOONEYRIVLINPARAMETERS)
+    ).calculate_cauchy_stress(&get_deformation_gradient()).iter()
+    .zip(
+        MooneyRivlinModel::new(MOONEYRIVLINPARAMETERS).calculate_cauchy_stress(&get_deformation_gradient()).iter()
+        .zip(GentModel::new(GENTPARAMETERS).calculate_cauchy_stress(&get_deformation_gradient()).iter())
+    ).for_each(|(cauchy_stress_i, (cauchy_stress_1_i, cauchy_stress_2_i))|
+        cauchy_stress_i.iter()
+        .zip(
+            cauchy_stress_1_i.iter()
+            .zip(cauchy_stress_2_i.iter())
+        ).for_each(|(cauchy_stress_ij, (cauchy_stress_1_ij, cauchy_stress_2_ij))|
+            assert_eq!(cauchy_stress_ij, &(cauchy_stress_1_ij + cauchy_stress_2_ij))
+        )
+    )
+}
+
+#[test]
+fn additive_cauchy_tangent_stiffness()
+{
+    CompositeHyperelasticConstitutiveModelEqualDeformation::construct(
+        GentModel::new(GENTPARAMETERS),
+        MooneyRivlinModel::new(MOONEYRIVLINPARAMETERS)
+    ).calculate_cauchy_tangent_stiffness(&get_deformation_gradient()).iter()
+    .zip(
+        MooneyRivlinModel::new(MOONEYRIVLINPARAMETERS).calculate_cauchy_tangent_stiffness(&get_deformation_gradient()).iter()
+        .zip(GentModel::new(GENTPARAMETERS).calculate_cauchy_tangent_stiffness(&get_deformation_gradient()).iter())
+    ).for_each(|(cauchy_tangent_stiffness_i, (cauchy_tangent_stiffness_1_i, cauchy_tangent_stiffness_2_i))|
+        cauchy_tangent_stiffness_i.iter()
+        .zip(
+            cauchy_tangent_stiffness_1_i.iter()
+            .zip(cauchy_tangent_stiffness_2_i.iter())
+        ).for_each(|(cauchy_tangent_stiffness_ij, (cauchy_tangent_stiffness_1_ij, cauchy_tangent_stiffness_2_ij))|
+            cauchy_tangent_stiffness_ij.iter()
+            .zip(
+                cauchy_tangent_stiffness_1_ij.iter()
+                .zip(cauchy_tangent_stiffness_2_ij.iter())
+            ).for_each(|(cauchy_tangent_stiffness_ijk, (cauchy_tangent_stiffness_1_ijk, cauchy_tangent_stiffness_2_ijk))|
+                cauchy_tangent_stiffness_ijk.iter()
+                .zip(
+                    cauchy_tangent_stiffness_1_ijk.iter()
+                    .zip(cauchy_tangent_stiffness_2_ijk.iter())
+                ).for_each(|(cauchy_tangent_stiffness_ijkl, (cauchy_tangent_stiffness_1_ijkl, cauchy_tangent_stiffness_2_ijkl))|
+                    assert_eq!(cauchy_tangent_stiffness_ijkl, &(cauchy_tangent_stiffness_1_ijkl + cauchy_tangent_stiffness_2_ijkl))
+                )
+            )
+        )
+    )
+}
+
+#[test]
+fn additive_helmholtz_free_energy_density()
+{
+    assert_eq!(
+        CompositeHyperelasticConstitutiveModelEqualDeformation::construct(
+            GentModel::new(GENTPARAMETERS),
+            MooneyRivlinModel::new(MOONEYRIVLINPARAMETERS)
+        ).calculate_helmholtz_free_energy_density(&get_deformation_gradient()),
+        MooneyRivlinModel::new(MOONEYRIVLINPARAMETERS).calculate_helmholtz_free_energy_density(&get_deformation_gradient())
+        + GentModel::new(GENTPARAMETERS).calculate_helmholtz_free_energy_density(&get_deformation_gradient())
+    )
 }
--- src/constitutive/hyperelastic/multiplicative_decomposition/test.rs
+++ src/constitutive/hyperelastic/multiplicative_decomposition/test.rs
@@ -1,13 +1,30 @@
-#[test]
-fn todo()
+use crate::
 {
-    todo!()
-}
+    constitutive::
+    {
+        GentModel,
+        MooneyRivlinModel,
+        NeoHookeanModel,
+        YeohModel,
+        hyperelastic::test::test_hyperelastic_constitutive_model_constructed,
+        test::
+        {
+            GENTPARAMETERS,
+            MOONEYRIVLINPARAMETERS,
+            NEOHOOKEANPARAMETERS,
+            YEOHPARAMETERS
+        }
+    },
+    mechanics::test::get_deformation_gradient
+};
+use super::*;
+
 #[test]
 fn dont_forget_to_test_mandel_stress_too()
 {
     todo!()
 }
+
 #[test]
 fn also_consider_the_combo_methods_returning_tuples()
 {
