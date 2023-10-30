dc5336e61f108a91f8bf126f4291c56be083bfdb
mrbuche <dev@mrbuche.com>
2023-10-30 10:47:09 -0600

    objective/residual/tangent seems OK (or at least consistent), mistake in optimization implementation?

--- src/constitutive/hyperelastic/multiplicative_decomposition/test.rs
+++ src/constitutive/hyperelastic/multiplicative_decomposition/test.rs
@@ -1,5 +1,6 @@
 use crate::
 {
+    EPSILON,
     constitutive::
     {
         GentModel,
@@ -41,16 +42,6 @@ fn also_consider_the_combo_methods_returning_tuples()
     todo!()
 }
 
-#[test]
-fn todo()
-{
-    todo!(
-        "Test objective, residual, etc. over finite difference, objectivity, etc. before moving on to full tests below.
-        
-        Can you mock a model with the needed functions to test using the macro?"
-    )
-}
-
 // mod dual
 // {
 //     use super::*;
@@ -70,6 +61,114 @@ fn get_composite_constitutive_model<'a>() -> CompositeHyperelasticConstitutiveMo
     )
 }
 
+fn calculate_residual_from_finite_difference_of_objective(is_deformed: bool) -> FirstPiolaKirchoffStress2
+{
+    let model = get_composite_constitutive_model();
+    let mut residual = FirstPiolaKirchoffStress2::zero();
+    let deformation_gradient =
+        if is_deformed
+        {
+            get_deformation_gradient()
+        }
+        else
+        {
+            DeformationGradient::identity()
+        };
+    for i in 0..3
+    {
+        for j in 0..3
+        {
+            let mut deformation_gradient_2_plus = 
+                if is_deformed
+                {
+                    get_deformation_gradient_2()
+                }
+                else
+                {
+                    DeformationGradient2::identity()
+                };
+            deformation_gradient_2_plus[i][j] += 0.5*EPSILON;
+            let objective_plus = model.calculate_objective(
+                &deformation_gradient,
+                &deformation_gradient_2_plus
+            );
+            let mut deformation_gradient_2_minus = 
+                if is_deformed
+                {
+                    get_deformation_gradient_2()
+                }
+                else
+                {
+                    DeformationGradient2::identity()
+                };
+            deformation_gradient_2_minus[i][j] -= 0.5*EPSILON;
+            let objective_minus = model.calculate_objective(
+                &deformation_gradient,
+                &deformation_gradient_2_minus
+            );
+            residual[i][j] = (objective_plus - objective_minus)/EPSILON;
+        }
+    }
+    residual
+}
+
+fn calculate_first_piola_kirchoff_tangent_stiffness_from_finite_difference_of_first_piola_kirchoff_stress(is_deformed: bool) -> FirstPiolaKirchoffTangentStiffness2
+{
+    let model = get_composite_constitutive_model();
+    let mut residual_tangent = FirstPiolaKirchoffTangentStiffness2::zero();
+    let deformation_gradient =
+        if is_deformed
+        {
+            get_deformation_gradient()
+        }
+        else
+        {
+            DeformationGradient::identity()
+        };
+    for k in 0..3
+    {
+        for l in 0..3
+        {
+            let mut deformation_gradient_2_plus = 
+                if is_deformed
+                {
+                    get_deformation_gradient_2()
+                }
+                else
+                {
+                    DeformationGradient2::identity()
+                };
+            deformation_gradient_2_plus[k][l] += 0.5*EPSILON;
+            let residual_plus = model.calculate_residual(
+                &deformation_gradient,
+                &deformation_gradient_2_plus
+            );
+            let mut deformation_gradient_2_minus = 
+                if is_deformed
+                {
+                    get_deformation_gradient_2()
+                }
+                else
+                {
+                    DeformationGradient2::identity()
+                };
+            deformation_gradient_2_minus[k][l] -= 0.5*EPSILON;
+            let residual_minus = model.calculate_residual(
+                &deformation_gradient,
+                &deformation_gradient_2_minus
+            );
+            for i in 0..3
+            {
+                for j in 0..3
+                {
+                    residual_tangent[i][j][k][l] = (residual_plus[i][j] - residual_minus[i][j])/EPSILON;
+                }
+            }
+        }
+    }
+    residual_tangent
+}
+
 mod objective
 {
     use super::*;
@@ -79,7 +178,18 @@ mod objective
         #[test]
         fn finite_difference()
         {
-            todo!()
+            get_composite_constitutive_model().calculate_residual(
+                &get_deformation_gradient(),
+                &get_deformation_gradient_2()
+            ).iter().zip(
+                calculate_residual_from_finite_difference_of_objective(true).iter()
+            ).for_each(|(residual_i, fd_residual_i)|
+                residual_i.iter()
+                .zip(fd_residual_i.iter())
+                .for_each(|(residual_ij, fd_residual_ij)|
+                    assert!((residual_ij/fd_residual_ij - 1.0).abs() < EPSILON)
+                )
+            )
         }
         #[test]
         fn objectivity()
@@ -102,7 +212,13 @@ mod objective
         #[test]
         fn finite_difference()
         {
-            todo!()
+            calculate_residual_from_finite_difference_of_objective(false).iter()
+            .for_each(|fd_residual_i|
+                fd_residual_i.iter()
+                .for_each(|fd_residual_ij|
+                    assert!(fd_residual_ij.abs() < EPSILON)
+                )
+            )
         }
         #[test]
         fn zero()
@@ -110,7 +226,7 @@ mod objective
             assert_eq!(
                 get_composite_constitutive_model().calculate_objective(
                     &DeformationGradient::identity(),
-                    &DeformationGradient::identity().into()
+                    &DeformationGradient2::identity()
                 ), 0.0
             )
         }
@@ -126,7 +242,26 @@ mod residual
         #[test]
         fn finite_difference()
         {
-            todo!()
+            get_composite_constitutive_model().calculate_residual_tangent(
+                &get_deformation_gradient(),
+                &get_deformation_gradient_2()
+            ).iter().zip(
+                calculate_first_piola_kirchoff_tangent_stiffness_from_finite_difference_of_first_piola_kirchoff_stress(true).iter()
+            ).for_each(|(residual_tangent_i, fd_residual_tangent_i)|
+                residual_tangent_i.iter()
+                .zip(fd_residual_tangent_i.iter())
+                .for_each(|(residual_tangent_ij, fd_residual_tangent_ij)|
+                    residual_tangent_ij.iter()
+                    .zip(fd_residual_tangent_ij.iter())
+                    .for_each(|(residual_tangent_ijk, fd_residual_tangent_ijk)|
+                        residual_tangent_ijk.iter()
+                        .zip(fd_residual_tangent_ijk.iter())
+                        .for_each(|(residual_tangent_ijkl, fd_residual_tangent_ijkl)|
+                            assert!((residual_tangent_ijkl/fd_residual_tangent_ijkl - 1.0).abs() < EPSILON)
+                        )
+                    )
+                )
+            )
         }
         #[test]
         fn objectivity()
@@ -157,14 +292,36 @@ mod residual
         #[test]
         fn finite_difference()
         {
-            todo!()
+            get_composite_constitutive_model().calculate_residual_tangent(
+                &DeformationGradient::identity(),
+                &DeformationGradient2::identity()
+            ).iter().zip(
+                calculate_first_piola_kirchoff_tangent_stiffness_from_finite_difference_of_first_piola_kirchoff_stress(false).iter()
+            ).for_each(|(residual_tangent_i, fd_residual_tangent_i)|
+                residual_tangent_i.iter()
+                .zip(fd_residual_tangent_i.iter())
+                .for_each(|(residual_tangent_ij, fd_residual_tangent_ij)|
+                    residual_tangent_ij.iter()
+                    .zip(fd_residual_tangent_ij.iter())
+                    .for_each(|(residual_tangent_ijk, fd_residual_tangent_ijk)|
+                        residual_tangent_ijk.iter()
+                        .zip(fd_residual_tangent_ijk.iter())
+                        .for_each(|(residual_tangent_ijkl, fd_residual_tangent_ijkl)|
+                            assert!(
+                                (residual_tangent_ijkl/fd_residual_tangent_ijkl - 1.0).abs() < EPSILON ||
+                                fd_residual_tangent_ijkl.abs() < EPSILON
+                            )
+                        )
+                    )
+                )
+            )
         }
         #[test]
         fn zero()
         {
             get_composite_constitutive_model().calculate_residual(
                 &DeformationGradient::identity(),
-                &DeformationGradient::identity().into()
+                &DeformationGradient2::identity()
             ).iter().for_each(|residual_i|
                 residual_i.iter().for_each(|residual_ij|
                     assert_eq!(residual_ij, &0.0)
@@ -249,7 +406,7 @@ mod residual_tangent
             let model = get_composite_constitutive_model();
             let residual_tangent = model.calculate_residual_tangent(
                 &DeformationGradient::identity(),
-                &DeformationGradient::identity().into()
+                &DeformationGradient2::identity()
             );
             for i in 0..3
             {
