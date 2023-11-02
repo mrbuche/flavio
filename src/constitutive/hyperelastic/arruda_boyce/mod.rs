56892b66b8236ff7b6950ccf6a3c557f9c78800b
mrbuche <dev@mrbuche.com>
2023-11-02 08:52:04 -0600

    tdd

--- src/constitutive/hyperelastic/arruda_boyce/mod.rs
+++ src/constitutive/hyperelastic/arruda_boyce/mod.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod test;
 
+use crate::math::inverse_langevin;
 use super::*;
 
 pub struct ArrudaBoyceModel<'a>
@@ -28,7 +29,12 @@ impl<'a> ConstitutiveModel<'a> for ArrudaBoyceModel<'a>
     }
     fn calculate_helmholtz_free_energy_density(&self, deformation_gradient: &DeformationGradient) -> Scalar
     {
-        todo!()
+        let jacobian = deformation_gradient.determinant();
+        let isochoric_left_cauchy_green_deformation = self.calculate_left_cauchy_green_deformation(deformation_gradient)/jacobian.powf(2.0/3.0);
+        let gamma = (isochoric_left_cauchy_green_deformation.trace()/3.0/self.get_number_of_links()).sqrt();
+        let eta = inverse_langevin(gamma);
+        self.get_shear_modulus() * self.get_number_of_links() * (gamma * eta - (eta.sinh() / eta).ln()) + 0.5 * self.get_bulk_modulus() * (0.5 * (jacobian.powi(2) - 1.0) - jacobian.ln())
+
     }
     fn new(parameters: ConstitutiveModelParameters<'a>) -> Self
     {
