074bec6e9f282cbad08236e48ed690a7f03f22f0
mrbuche <dev@mrbuche.com>
2023-11-07 20:56:01 -0700

    some more tests

--- src/math/tensor/rank_0/list/test.rs
+++ src/math/tensor/rank_0/list/test.rs
@@ -1,53 +1,108 @@
+use super::
+{
+    TensorRank0,
+    TensorRank0List,
+    TensorRank0ListTrait
+};
+
+fn get_array() -> [TensorRank0; 4]
+{
+    [1.0, 2.0, 3.0, 4.0]
+}
+
+fn get_tensor_rank_0_list() -> TensorRank0List<4>
+{
+    TensorRank0List::new(get_array())
+}
+
+fn get_other_tensor_rank_0_list() -> TensorRank0List<4>
+{
+    TensorRank0List::new([5.0, 6.0, 7.0, 8.0])
+}
+
 #[test]
 fn from_iter()
 {
-    todo!()
+    let into_iterator = (0..8).map(|x| x as TensorRank0).into_iter();
+    let tensor_rank_0_list = TensorRank0List::<8>::from_iter(into_iterator.clone());
+    tensor_rank_0_list.iter()
+    .zip(into_iterator)
+    .for_each(|(tensor_rank_0_list_i, value_i)|
+        assert_eq!(tensor_rank_0_list_i, &value_i)
+    );
 }
 
 #[test]
 fn iter()
 {
-    todo!()
+    get_tensor_rank_0_list().iter()
+    .zip(get_array().iter())
+    .for_each(|(tensor_rank_0_list_i, array_i)|
+        assert_eq!(tensor_rank_0_list_i, array_i)
+    );
 }
 
 #[test]
 fn iter_mut()
 {
-    todo!()
+    get_tensor_rank_0_list().iter_mut()
+    .zip(get_array().iter_mut())
+    .for_each(|(tensor_rank_0_list_i, array_i)|
+        assert_eq!(tensor_rank_0_list_i, array_i)
+    );
 }
 
 #[test]
 fn mul_tensor_rank_1_list_to_self()
 {
-    todo!()
+    assert_eq!(
+        get_tensor_rank_0_list() * get_other_tensor_rank_0_list(),
+        70.0
+    )
 }
 
 #[test]
 fn mul_tensor_rank_1_list_ref_to_self()
 {
-    todo!()
+    assert_eq!(
+        get_tensor_rank_0_list() * &get_other_tensor_rank_0_list(),
+        70.0
+    )
 }
 
 #[test]
 fn mul_tensor_rank_1_list_to_self_ref()
 {
-    todo!()
+    assert_eq!(
+        &get_tensor_rank_0_list() * get_other_tensor_rank_0_list(),
+        70.0
+    )
 }
 
 #[test]
 fn mul_tensor_rank_1_list_ref_to_self_ref()
 {
-    todo!()
+    assert_eq!(
+        &get_tensor_rank_0_list() * &get_other_tensor_rank_0_list(),
+        70.0
+    )
 }
 
 #[test]
 fn new()
 {
-    todo!()
+    get_tensor_rank_0_list().iter()
+    .zip(get_array().iter())
+    .for_each(|(tensor_rank_0_list_i, array_i)|
+        assert_eq!(tensor_rank_0_list_i, array_i)
+    );
 }
 
 #[test]
 fn zero()
 {
-    todo!()
+    TensorRank0List::<8>::zero().iter()
+    .for_each(|tensor_rank_0_list_i|
+        assert_eq!(tensor_rank_0_list_i, &0.0)
+    );
 }
--- src/math/tensor/rank_2/list_2d/test.rs
+++ src/math/tensor/rank_2/list_2d/test.rs
@@ -1,29 +1,140 @@
+use super::
+{
+    TensorRank0,
+    TensorRank2List2D,
+    TensorRank2List2DTrait
+};
+
+fn get_array() -> [[[[TensorRank0; 3]; 3]; 2]; 2]
+{
+    [[[
+        [1.0, 4.0, 6.0],
+        [7.0, 2.0, 5.0],
+        [9.0, 8.0, 3.0]
+    ],[
+        [3.0, 2.0, 3.0],
+        [6.0, 5.0, 2.0],
+        [4.0, 5.0, 0.0]
+    ]], [[
+        [5.0, 2.0, 9.0],
+        [2.0, 4.0, 5.0],
+        [1.0, 3.0, 8.0]
+    ],[
+        [4.0, 3.0, 2.0],
+        [2.0, 5.0, 4.0],
+        [1.0, 7.0, 1.0]
+    ]]]
+}
+
+fn get_tensor_rank_2_list_2d() -> TensorRank2List2D<3, 1, 1, 2>
+{
+    TensorRank2List2D::new(get_array())
+}
+
 #[test]
 fn from_iter()
 {
-    todo!()
+    let into_iterator = get_tensor_rank_2_list_2d().0.into_iter();
+    let tensor_rank_2_list_2d = TensorRank2List2D::<3, 1, 1, 2>::from_iter(get_tensor_rank_2_list_2d().0.into_iter());
+    tensor_rank_2_list_2d.iter()
+    .zip(into_iterator)
+    .for_each(|(tensor_rank_2_list_2d_i, array_i)|
+        tensor_rank_2_list_2d_i.iter()
+        .zip(array_i.iter())
+        .for_each(|(tensor_rank_2_list_2d_ij, array_ij)|
+            tensor_rank_2_list_2d_ij.iter()
+            .zip(array_ij.iter())
+            .for_each(|(tensor_rank_2_list_2d_ij_w, array_ij_w)|
+                tensor_rank_2_list_2d_ij_w.iter()
+                .zip(array_ij_w.iter())
+                .for_each(|(tensor_rank_2_list_2d_ij_ww, array_ij_ww)|
+                    assert_eq!(tensor_rank_2_list_2d_ij_ww, array_ij_ww)
+                )
+            )
+        )
+    );
 }
 
 #[test]
 fn iter()
 {
-    todo!()
+    get_tensor_rank_2_list_2d().iter()
+    .zip(get_array().iter())
+    .for_each(|(tensor_rank_2_list_2d_i, array_i)|
+        tensor_rank_2_list_2d_i.iter()
+        .zip(array_i.iter())
+        .for_each(|(tensor_rank_2_list_2d_ij, array_ij)|
+            tensor_rank_2_list_2d_ij.iter()
+            .zip(array_ij.iter())
+            .for_each(|(tensor_rank_2_list_2d_ij_w, array_ij_w)|
+                tensor_rank_2_list_2d_ij_w.iter()
+                .zip(array_ij_w.iter())
+                .for_each(|(tensor_rank_2_list_2d_ij_ww, array_ij_ww)|
+                    assert_eq!(tensor_rank_2_list_2d_ij_ww, array_ij_ww)
+                )
+            )
+        )
+    );
 }
 
 #[test]
 fn iter_mut()
 {
-    todo!()
+    get_tensor_rank_2_list_2d().iter_mut()
+    .zip(get_array().iter_mut())
+    .for_each(|(tensor_rank_2_list_2d_i, array_i)|
+        tensor_rank_2_list_2d_i.iter_mut()
+        .zip(array_i.iter_mut())
+        .for_each(|(tensor_rank_2_list_2d_ij, array_ij)|
+            tensor_rank_2_list_2d_ij.iter_mut()
+            .zip(array_ij.iter_mut())
+            .for_each(|(tensor_rank_2_list_2d_ij_w, array_ij_w)|
+                tensor_rank_2_list_2d_ij_w.iter_mut()
+                .zip(array_ij_w.iter_mut())
+                .for_each(|(tensor_rank_2_list_2d_ij_ww, array_ij_ww)|
+                    assert_eq!(tensor_rank_2_list_2d_ij_ww, array_ij_ww)
+                )
+            )
+        )
+    );
 }
 
 #[test]
 fn new()
 {
-    todo!()
+    get_tensor_rank_2_list_2d().iter()
+    .zip(get_array().iter())
+    .for_each(|(tensor_rank_2_list_2d_i, array_i)|
+        tensor_rank_2_list_2d_i.iter()
+        .zip(array_i.iter())
+        .for_each(|(tensor_rank_2_list_2d_ij, array_ij)|
+            tensor_rank_2_list_2d_ij.iter()
+            .zip(array_ij.iter())
+            .for_each(|(tensor_rank_2_list_2d_ij_w, array_ij_w)|
+                tensor_rank_2_list_2d_ij_w.iter()
+                .zip(array_ij_w.iter())
+                .for_each(|(tensor_rank_2_list_2d_ij_ww, array_ij_ww)|
+                    assert_eq!(tensor_rank_2_list_2d_ij_ww, array_ij_ww)
+                )
+            )
+        )
+    );
 }
 
 #[test]
 fn zero()
 {
-    todo!()
+    TensorRank2List2D::<3, 1, 1, 8>::zero().iter()
+    .for_each(|tensor_rank_0_list_i|
+        tensor_rank_0_list_i.iter()
+        .for_each(|tensor_rank_0_list_ij|
+            tensor_rank_0_list_ij.iter()
+            .for_each(|tensor_rank_0_list_ij_w|
+                tensor_rank_0_list_ij_w.iter()
+                .for_each(|tensor_rank_0_list_ij_ww|
+                    assert_eq!(tensor_rank_0_list_ij_ww, &0.0)
+                )
+            )
+        )
+    );
 }
