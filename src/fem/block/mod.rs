a04921f4a9a6313b5195d6f70effb33d3303a2ca
mrbuche <dev@mrbuche.com>
2023-11-08 18:20:33 -0700

    block

--- src/fem/block/mod.rs
+++ src/fem/block/mod.rs
@@ -1,3 +1,6 @@
+#[cfg(test)]
+mod test;
+
 pub mod element;
 
 use crate::
@@ -28,3 +31,31 @@ use crate::
         Vectors
     }
 };
+use self::element::FiniteElement;
+
+type CurrentNodalCoordinates<const D: usize> = CurrentCoordinates<D>;
+type NodalForces<const D: usize> = Forces<D>;
+type NodalStiffnesses<const D: usize> = Stiffnesses<D>;
+type ReferenceNodalCoordinates<const D: usize> = ReferenceCoordinates<D>;
+
+pub struct FiniteElementBlock<'a, C, const D: usize, const E: usize, F, const G: usize, const N: usize>
+where
+    C: ConstitutiveModel<'a>,
+    F: FiniteElement<'a, C, G, N>
+{
+    elements: [F; E],
+    phantom_a: std::marker::PhantomData<*const &'a C>
+}
+
+pub trait FiniteElementBlockTraits<'a, C, const D: usize, const E: usize, F, const G: usize, const N: usize>
+where
+    C: ConstitutiveModel<'a>,
+    F: FiniteElement<'a, C, G, N>
+{
+    fn calculate_helmholtz_free_energy(&self) -> Scalar;
+    fn calculate_nodal_forces(&self) -> NodalForces<D>;
+    fn calculate_nodal_stiffnesses(&self) -> NodalStiffnesses<D>;
+    fn get_current_nodal_coordinates(&self) -> &CurrentNodalCoordinates<D>;
+    fn get_elements(&self) -> &[F; E];
+    fn set_current_nodal_coordinates(&mut self, current_nodal_coordinates: CurrentNodalCoordinates<D>);
+}
---
+++ src/fem/block/test.rs
@@ -0,0 +1,5 @@
+#[test]
+fn block_tests_by_element()
+{
+    todo!("You shall not pass!")
+}
