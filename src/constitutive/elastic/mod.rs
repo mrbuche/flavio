68d098c2c3827d0a926143f0b651348b6865fda8
mrbuche <dev@mrbuche.com>
2024-01-24 09:42:32 -0700

    this will work

--- src/constitutive/elastic/mod.rs
+++ src/constitutive/elastic/mod.rs
@@ -1,5 +1,4 @@
 //! Elastic constitutive models cannot be defined by a Helmholtz free energy density but still depend on only the deformation gradient. These constitutive models are therefore defined by a relation for some stress measure as a function of the deformation gradient. Consequently, the tangent stiffness associated with the first Piola-Kirchoff stress is not symmetric for elastic constitutive models.
-//!
 //! ```math
 //! \mathcal{C}_{iJkL} \neq \mathcal{C}_{kLiJ}
 //! ```
