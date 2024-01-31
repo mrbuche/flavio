053e8d83d88e92a5102eb61b9d843687802e4808
mrbuche <dev@mrbuche.com>
2024-01-31 12:20:35 -0700

    simple for now

--- src/constitutive/thermal/conduction/fourier/test.rs
+++ src/constitutive/thermal/conduction/fourier/test.rs
@@ -1,5 +1,19 @@
-#[test]
-fn todo()
+use super::
 {
-    todo!()
+    ConstitutiveModel,
+    FourierModel,
+    TemperatureGradient,
+    ThermalConduction,
+    super::test::FOURIERPARAMETERS
+};
+use crate::math::TensorRank1Trait;
+
+#[test]
+fn zero()
+{
+    let model = FourierModel::new(FOURIERPARAMETERS);
+    let zero_vector = TemperatureGradient::new([0.0, 0.0, 0.0]);
+    model.calculate_heat_flux(&zero_vector).iter().for_each(|entry|
+        assert_eq!(entry, &0.0)
+    )
 }
--- src/constitutive/thermal/conduction/test.rs
+++ src/constitutive/thermal/conduction/test.rs
@@ -1,8 +1,2 @@
 use crate::mechanics::Scalar;
 pub const FOURIERPARAMETERS: &[Scalar; 1] = &[1.0];
-
-#[test]
-fn todo()
-{
-    todo!()
-}
