4b0f135550f5f239084f13c8617a5fd0f498a621
mrbuche <dev@mrbuche.com>
2024-11-01 11:40:07 -0600

    weird that not unwrapping results can be so slow

--- src/math/optimize/gradient_descent/test.rs
+++ src/math/optimize/gradient_descent/test.rs
@@ -1,4 +1,42 @@
+use super::{FirstOrder, GradientDescent, TensorRank0};
+
+const TOLERANCE: TensorRank0 = 1e-6;
+
 #[test]
-fn todo() {
-    todo!()
+fn linear() {
+    assert!(
+        GradientDescent {
+            ..Default::default()
+        }
+        .minimize(|x: &TensorRank0| *x, 1.0,)
+        .unwrap()
+        .abs()
+            < TOLERANCE
+    )
+}
+
+#[test]
+fn quadratic() {
+    assert!(
+        GradientDescent {
+            ..Default::default()
+        }
+        .minimize(|x: &TensorRank0| x.powi(2) / 2.0, 1.0,)
+        .unwrap()
+        .abs()
+            < TOLERANCE
+    )
+}
+
+#[test]
+fn sin() {
+    assert!(
+        GradientDescent {
+            ..Default::default()
+        }
+        .minimize(|x: &TensorRank0| x.sin(), 1.0,)
+        .unwrap()
+        .abs()
+            < TOLERANCE
+    )
 }
